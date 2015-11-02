dab728e497d3987840f9f9bbfb4b9ab17311870e
kntmrkm <kntmrkm@gmail.com>
2015-11-02 23:04:14 +0900

    update

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -1,5 +1,8 @@
 //= require jquery/dist/jquery.js
 //= require echojs/dist/echo.js
+//= require moment/min/moment.min.js
+//= require moment/locale/ja.js
+//= require jquery-rss/dist/jquery.rss.min.js
 //= require bootstrap-sprockets
 //= require slidebars/distribution/0.10.2/slidebars.js
 //= require slidebars
@@ -7,5 +10,17 @@
 
 $(function() {
   init_slidebars();
+  initJqueryRSS();
 });
 
+initJqueryRSS = function() {
+  $("#blog-rss-feeds").rss("http://www.specificfeeds.com/kntmrkm-new",
+    {
+      limit: 10,
+      layoutTemplate: "<ul class='list-group'>{entries}</ul>",
+      entryTemplate: "<li class='list-group-item'><a href='{url}'>{date} {title}</a></li>",
+      dateFormat: 'YYYY/MM/DD'
+    })
+
+
+};
